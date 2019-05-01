74f07aeb38442c021db7f948102ee85f0f576686
Vesa Piittinen <vesa.piittinen@gmail.com>
2019-05-01 16:12:06 +0300

    chore: build UMD with terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,45 @@
-// import babel from 'rollup-plugin-babel'
 import json from 'rollup-plugin-json'
+import { terser } from 'rollup-plugin-terser'
 import { uglify } from 'rollup-plugin-uglify'
 
-export default {
-    //input: 'src/index.js',
-    input: 'lib/es/index.js',
-    output: {
-        file: 'lib/umd/nom.js',
-        format: 'umd',
-        name: 'NomJS'
+export default [
+    // create the main ES5 version with Terser
+    {
+        input: 'lib/es/index.js',
+        output: {
+            file: 'lib/umd/nom.js',
+            format: 'umd',
+            name: 'NomJS'
+        },
+        plugins: [
+            json(),
+            terser()
+        ]
     },
-    plugins: [
-        json(),
-/*
-        babel({
-            exclude: '{node_modules|__tests__}/**'
-        }),
-*/
-        uglify()
-    ]
-}
+    // alternate ES5 version with Uglify
+    {
+        input: 'lib/es/index.js',
+        output: {
+            file: 'lib/umd/nom.es5.js',
+            format: 'umd',
+            name: 'NomJS'
+        },
+        plugins: [
+            json(),
+            uglify()
+        ]
+    },
+    // ES6 version with Terser
+    {
+        input: 'src/index.js',
+        output: {
+            file: 'lib/umd/nom.es6.js',
+            format: 'umd',
+            name: 'NomJS'
+        },
+        plugins: [
+            json(),
+            terser()
+        ]
+    },
+]
